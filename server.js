2ea8de00899f055f0a18ace1d95b43a704de2c77
asakuya <yasutomo1020@gmail.com>
2023-06-08 20:52:24 +0900

    コメント追加

--- server.js
+++ server.js
@@ -1,26 +1,35 @@
-//import { Configuration, OpenAIApi } from "openai";
 import {
     Configuration,
     OpenAIApi,
     ChatCompletionRequestMessageRoleEnum,
-} from "openai";
+} from "openai";// OpenAIのライブラリをインポートします。
 import express from 'express'; // expressをインポートします。
 import bodyParser from 'body-parser'; // body-parserをインポートします。
 
+// expressを初期化します。
 const app = express();
+// body-parserを初期化します。
 app.use(bodyParser.text());
-//app.use(express.text()); // JSONのパースを有効にします。
+
+// OpenAIのAPIを利用するための設定を行います。
 const configuration = new Configuration({
     organization: "org-O3dmbCRpQmzrQHvcwVlAMI0I",
     apiKey: process.env.OPENAI_API_KEY,
 });
+
+// 環境変数OPENAI_API_KEYが設定されているかを確認します。
 if (process.env.OPENAI_API_KEY === undefined) {
     console.log("環境変数OPENAI_API_KEYが設定されていません。");
 }
+
+// 環境変数確認
 console.log(process.env.OPENAI_API_KEY);
+
+// 初期化
 const openai = new OpenAIApi(configuration);
 //const response = await openai.listEngines();
 
+// クライアントからのリクエストを受け取り、OpenAIのAPIを利用して応答を返します。
 app.post('/chat', async (req, res) => {
     const content = req.body;
     console.log(content);
@@ -34,7 +43,12 @@ app.post('/chat', async (req, res) => {
     //   temperature: 0.7
 });
 
-app.use(express.static('public')); // 静的ファイル（この場合はHTML）を提供するための設定です。
+// 静的ファイル（この場合はHTML）を提供するための設定です。
+app.use(express.static('public'));
+
+// サーバーを起動します。
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server running on port ${port}`));
-app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html')); // ルートパスへのリクエストに対してindex.htmlを返すように設定します。
+
+// ルートパスへのリクエストに対してindex.htmlを返すように設定します。
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html')); 
